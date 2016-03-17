1ba4c5653d06c9aeff803156f76cb0c72dcd030f
Matt Gleason <gleason.matt1987@gmail.com>
2016-03-17 14:49:00 -0600

    forgot comma

--- client/views/users/login.js
+++ client/views/users/login.js
@@ -10,7 +10,7 @@ Template.login.events({
             email: email,
             password: password
     	});
-    }
+    },
 
     'submit .login': function(event){
     	var email = event.target.email;
