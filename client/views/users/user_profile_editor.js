45cb62b47868d7d9d3895d036c16430b81fa7708
plstate <plstate@gmail.com>
2016-03-26 14:01:54 -0400

    more styling work done on the user profile editor, but now the "ghosting" issue has returned

--- client/views/users/user_profile_editor.js
+++ client/views/users/user_profile_editor.js
@@ -1,6 +1,1 @@
-Template.userProfileEditor.onRendered(function() {
-	// for some reason, the content was "ghosting" (i.e. showing up more
-	// than once), so until we figure out what's causing it, we will make
-	// the offending element disappear
-	$("#sky-header .title-bar #my-profile").hide();
-});
+// todo
