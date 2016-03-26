2a8ed8a6ebc861cea4f43efc2435d6e369c8137f
plstate <plstate@gmail.com>
2016-03-26 13:56:00 -0400

    more work done on the profile editor (though it's not styled or functional yet)

--- client/views/users/user_profile_editor.js
+++ client/views/users/user_profile_editor.js
@@ -1,3 +1,6 @@
-Template.userProfileEditor.helpers({
-	// todo
+Template.userProfileEditor.onRendered(function() {
+	// for some reason, the content was "ghosting" (i.e. showing up more
+	// than once), so until we figure out what's causing it, we will make
+	// the offending element disappear
+	$("#sky-header .title-bar #my-profile").hide();
 });
