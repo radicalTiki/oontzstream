46950105f8252569867a86b40b1b4ee4d9df511b
plstate <plstate@gmail.com>
2016-03-27 22:08:09 -0400

    admin panel work - now only admins can access it

--- client/views/admin/admin_panel.js
+++ client/views/admin/admin_panel.js
@@ -1,3 +1,11 @@
 Template.adminPanel.onRendered(function() {
+	// do the user list first thing after load
+	Meteor.users.find().fetch().forEach(function(u) {
+		var userListDivContents = $(".admin-user-list").html();
+		console.log(userListDivContents); // debug
+		$(".admin-user-list").html(userListDivContents + "<p id=\"user-" + u._id + "\" class=\"user-list-line\">" + u.username + "</p>");
+	});
+	
+	// now clean up any ghosting that occurs
 	$("#sky-header #admin-panel").remove();
 });
