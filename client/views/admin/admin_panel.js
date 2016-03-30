92995ac53a8c91d1678ed7e4dade5a947c3364f8
plstate <plstate@gmail.com>
2016-03-29 22:24:49 -0400

    Added functionality for the (future) room list in the admin panel

--- client/views/admin/admin_panel.js
+++ client/views/admin/admin_panel.js
@@ -5,4 +5,10 @@ Template.adminPanel.onRendered(function() {
 		console.log(userListDivContents); // debug
 		$(".admin-user-list").html(userListDivContents + "<p id=\"user-" + u._id + "\" class=\"user-list-line\">" + u.username + "</p>");
 	});
+	
+	// do the room list as well
+	Rooms.find().fetch().forEach(function(r) {
+		var roomListDivContents = $(".admin-room-list").html();
+		$(".admin-room-list").html(roomListDivContents + "<p id=\"room-" + r._id + "\" class=\"room-list-line\">" + r.title + "</p>");
+	});
 });
